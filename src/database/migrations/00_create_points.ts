37367baea9e34fe4514a11bb202cb8ec948d806b
Maicon Boer <maicon_boer@hotmail.com>
2020-06-02 18:34:01 -0300

    Att code

--- src/database/migrations/00_create_points.ts
+++ src/database/migrations/00_create_points.ts
@@ -2,7 +2,7 @@ import Knex from 'knex'
 
 export async function up(knex: Knex) {
     // criar tabela
-    return knex.schema.createTable('point', table => {
+    return knex.schema.createTable('points', table => {
         table.increments('id').primary()
         table.string('image').notNullable()
         table.string('name').notNullable()
@@ -17,7 +17,7 @@ export async function up(knex: Knex) {
 
 export async function down(knex: Knex) {
     // (deletar a tabela)
-    return knex.schema.dropTable('point')
+    return knex.schema.dropTable('points')
 }
 
 
--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,16 @@
-import express from 'express'
+import express, { request, response } from 'express'
+
+import PointsController from './controllers/PointsController'
+import ItemsController from './controllers/ItemsController'
 
 const routes = express.Router()
+const pointsController = new PointsController()
+const itemsController = new ItemsController()
 
-routes.get('/', (request, response) => {
-    return response.json({message: "Hello World! "})
-})
+routes.get('/items', itemsController.index)
 
-export default routes;
+routes.post('/points', pointsController.create)
+routes.get('/points', pointsController.index)
+routes.get('/points/:id', pointsController.show)
+
+export default routes; 
